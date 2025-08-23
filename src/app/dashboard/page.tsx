ec0da62e92ce5924c48f97518b0d10cc890e4979
Fusan02 <saitoufutoshi0909@gmail.com>
2025-08-24 05:28:53 +0900

    l -> L に修正

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import { createClient } from '@/lib/supabase/server'
 import { redirect } from 'next/navigation'
-import Logout from './components/logout'
+import Logout from './components/Logout'
 
 export default async function DashboardPage() {
   const supabase = await createClient()
